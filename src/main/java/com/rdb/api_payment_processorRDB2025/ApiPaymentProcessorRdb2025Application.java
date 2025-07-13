fd72760c6a313118843698b5fedead10f756dc2f
anadevti <anacarolayne777@gmail.com>
2025-07-12 22:43:17 -0300

    first commit

---
+++ src/main/java/com/rdb/api_payment_processorRDB2025/ApiPaymentProcessorRdb2025Application.java
@@ -0,0 +1,13 @@
+package com.rdb.api_payment_processorRDB2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPaymentProcessorRdb2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPaymentProcessorRdb2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rdb/api_payment_processorRDB2025/ApiPaymentProcessorRdb2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdb.api_payment_processorRDB2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPaymentProcessorRdb2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
